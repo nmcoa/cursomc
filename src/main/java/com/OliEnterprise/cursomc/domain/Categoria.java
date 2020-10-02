c3862d47fe016a35660c70e37d56802844343a90
nmcoa <nmcoa@iscte-iul.pt>
2020-10-02 17:26:46 +0100

    Banco de dados H2 e criação da base de dados

--- src/main/java/com/OliEnterprise/cursomc/domain/Categoria.java
+++ src/main/java/com/OliEnterprise/cursomc/domain/Categoria.java
@@ -1,7 +1,18 @@
 package com.OliEnterprise.cursomc.domain;
 
-public class Categoria {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String nome;
 	
@@ -28,6 +39,34 @@ public class Categoria {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
 	}
 	
 	
