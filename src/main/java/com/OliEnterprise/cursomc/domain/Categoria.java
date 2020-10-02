e4077492acd5bbf2c08dde013bc899bfa415b16e
nmcoa <nmcoa@iscte-iul.pt>
2020-10-02 16:50:03 +0100

    Rest testado

---
+++ src/main/java/com/OliEnterprise/cursomc/domain/Categoria.java
@@ -0,0 +1,34 @@
+package com.OliEnterprise.cursomc.domain;
+
+public class Categoria {
+
+	private int id;
+	private String nome;
+	
+	public Categoria() {
+	}
+
+	public Categoria(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
---
+++ src/main/java/com/OliEnterprise/cursomc/resources/Teste.java
@@ -0,0 +1,18 @@
+package com.OliEnterprise.cursomc.resources;
+
+public class Teste {
+
+	public Teste() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Cabuflamanau minino";
+	}
+
+	
+	
+	
+}
